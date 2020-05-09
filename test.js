cc8d84d8e9c594664e318cd6726d0f1efdf8a2a2
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-08 22:43:34 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,5 @@
+var nombre = "juan";
+var apellido = "herrera";
+var edad = 18;
+var texto = "quiero contar la historia de " + nombre + " " + apellido + " donde podemos\ncontar que es una persona que tiene " + edad;
+console.log(texto);
---
+++ test.ts
@@ -0,0 +1,9 @@
+
+let nombre: string = "juan";
+let apellido: string = "herrera";
+let edad: number = 18;
+
+let texto = `quiero contar la historia de ${nombre} ${apellido} donde podemos
+contar que es una persona que tiene ${edad}`;
+
+console.log(texto);
